9058510a26b2c37d33c365045520209aa69b1530
wenmingvs <wenmingvs@gmail.com>
2016-05-04 12:13:28 +0800

    【其他】欢迎界面的实现

---
+++ weiSwift/src/main/java/com/wenming/weiswift/WelcomeActivity.java
@@ -0,0 +1,42 @@
+package com.wenming.weiswift;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by wenmingvs on 16/5/4.
+ */
+public class WelcomeActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_layout);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                mHandler.sendMessage(Message.obtain());
+            }
+        }, 2000);
+    }
+
+
+    public Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
+
+
+}
