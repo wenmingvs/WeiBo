c76a79515e4555a1d128a53a5b53cfee8c1b57c4
郭文明 <gwm@meitu.com>
2016-08-09 18:42:18 +0800

    修复全局侧滑太灵敏，与手势冲突的问题

--- weiSwift/src/main/java/com/wenming/weiswift/ui/common/BaseSwipeActivity.java
+++ weiSwift/src/main/java/com/wenming/weiswift/ui/common/BaseSwipeActivity.java
@@ -2,19 +2,19 @@ package com.wenming.weiswift.ui.common;
 
 import android.os.Bundle;
 
-import me.imid.swipebacklayout.lib.SwipeBackLayout;
-import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
+import com.wenming.swipebacklayout.SwipeBackLayout;
+import com.wenming.swipebacklayout.app.SwipeBackActivity;
+
 
 /**
  * Created by wenmingvs on 2016/6/27.
  */
 public class BaseSwipeActivity extends SwipeBackActivity {
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSwipeBackLayout().setSwipeMode(SwipeBackLayout.FULL_SCREEN_LEFT);
         getSwipeBackLayout().setEdgeTrackingEnabled(SwipeBackLayout.EDGE_LEFT);
-        //getSwipeBackLayout().setSensitivity(BaseSwipeActivity.this, 0.7f);
+        getSwipeBackLayout().setSensitivity(BaseSwipeActivity.this, 0.3f);
     }
 }
