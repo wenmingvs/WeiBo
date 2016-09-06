fc896d4f26a82f6b5e8493034073071a7030655d
郭文明 <gwm@meitu.com>
2016-09-06 19:33:05 +0800

    支持选择时间线的图片质量

--- weiSwift/src/main/java/com/wenming/weiswift/ui/login/fragment/home/timelineimagelist/ImageAdapter.java
+++ weiSwift/src/main/java/com/wenming/weiswift/ui/login/fragment/home/timelineimagelist/ImageAdapter.java
@@ -33,7 +33,7 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> 
      * 用于加载微博列表图片的配置，进行安全压缩，尽可能的展示图片细节
      */
     private DisplayImageOptions options = new DisplayImageOptions.Builder()
-            .imageScaleType(ImageScaleType.EXACTLY)
+            .imageScaleType(ImageScaleType.IN_SAMPLE_POWER_OF_2)
             .bitmapConfig(Bitmap.Config.RGB_565)
             .cacheInMemory(true)
             .cacheOnDisk(true)
