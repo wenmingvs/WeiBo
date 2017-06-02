ce69e6f6d4b30850efcaf8ed087d985848334a76
wenmingvs <wenmingvs@foxmail.com>
2017-06-02 11:43:15 +0800

    asdasdsad

--- weiSwift/src/main/java/com/wenming/weiswift/app/common/oauth/constant/AppAuthConstants.java
+++ weiSwift/src/main/java/com/wenming/weiswift/app/common/oauth/constant/AppAuthConstants.java
@@ -7,5 +7,7 @@ package com.wenming.weiswift.app.common.oauth.constant;
 public class AppAuthConstants {
     public static final String APP_KEY = "4037909131";
     public static final String REDIRECT_URL = "https://api.weibo.com/oauth2/default.html";
-    public static final String SCOPE = "all";
+    public static final String SCOPE = "email,direct_messages_read,direct_messages_write,"
+            + "friendships_groups_read,friendships_groups_write,statuses_to_me_read,"
+            + "follow_app_official_microblog," + "invitation_write";
 }
