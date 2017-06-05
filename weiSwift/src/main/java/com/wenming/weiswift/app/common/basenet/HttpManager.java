bcebc69461c9ed46c2ac783079dd91a57293b93e
郭文明 <gwm@meitu.com>
2017-06-05 21:02:03 +0800

    asdasdasd

---
+++ weiSwift/src/main/java/com/wenming/weiswift/app/common/basenet/HttpManager.java
@@ -0,0 +1,8 @@
+package com.wenming.weiswift.app.common.basenet;
+
+/**
+ * Created by wenmingvs on 2017/6/5.
+ */
+
+public class HttpManager {
+}
---
+++ weiSwift/src/main/java/com/wenming/weiswift/app/home/net/HomeHttpHelper.java
@@ -0,0 +1,8 @@
+package com.wenming.weiswift.app.home.net;
+
+/**
+ * Created by wenmingvs on 2017/6/5.
+ */
+
+public class HomeHttpHelper {
+}
