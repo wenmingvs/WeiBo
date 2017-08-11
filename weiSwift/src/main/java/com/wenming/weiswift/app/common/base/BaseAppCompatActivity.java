1b867f3a1b5c3b23c1365b4cf925d6c7e1916848
wenmingvs <wenmingvs@foxmail.com>
2017-08-11 19:20:33 +0800

    asdasdasd

--- weiSwift/src/main/java/com/wenming/weiswift/app/common/base/BaseAppCompatActivity.java
+++ weiSwift/src/main/java/com/wenming/weiswift/app/common/base/BaseAppCompatActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.jaeger.library.StatusBarUtil;
 import com.wenming.weiswift.app.utils.ToastUtils;
 
 /**
